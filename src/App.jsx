b01b6ffd3a0ebbffc2bd15d56ec5bf45a68512a5
Gaurav Sharma <sharmag8121@gmail.com>
2024-02-14 01:51:45 +0530

    Static App List of Friends

--- src/App.jsx
+++ src/App.jsx
@@ -36,15 +36,22 @@ function FriendList() {
   );
 }
 
-function Friend({ friend }) {
-  return <li>
-    <img src={friend.image} alt={friend.name} />
-    <h3>{friend.name}</h3>
+function Friend({ friend, onSelection, selectedFriend }) {
+  const isSelected = selectedFriend?.id === friend.id;
 
-    {friend.balance < 0 && (<p className="red">You owe {friend.name} ${Maths.abs(friend.balance)}€</p>)}
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
 
-    {friend.balance > 0 && (<p className="green">You owe {friend.name} ${Maths.abs(friend.balance)}€</p>)}
-
-    {friend.balance === 0 && (<p>You owe {friend.name} ${Maths.abs(friend.balance)}€</p>)}
-  </li>;
+      {friend.balance < 0 && (<p className="red">
+        You owe {friend.name} {Math.abs(friend.balance)} €
+      </p>)}
+      {friend.balance > 0 && (<p className="green">
+        You owe {friend.name} owes you {Math.abs(friend.balance)} €
+      </p>
+      )}
+      {friend.balance === 0 && <p> You and {friend.name} are even</p>}
+    </li>
+  );
 }
